349bbeee8422539023a22309de783abeab5150b5
maxdemo1 <maxbrownwot@gmail.com>
2024-05-17 22:20:38 +0300

    v1_fix

--- schemas/contactsMongooseSchemas.js
+++ schemas/contactsMongooseSchemas.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false }
+);
 
 export default mongoose.model("contact", contactSchema);
